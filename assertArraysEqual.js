e1ae13c2926c331c6aaf1347dd2e003245699d46
Aiman03at <aiman03at@gmail.com>
2024-05-21 14:36:01 -0400

    Asertion message for comparing two arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,37 @@
+// Input two arrays
+//Output appropriate message
+//checking two arrays are equal
+//Using already implemented eqArrays
+
+// Function to evaluate whether two arrays are a perfect match
+function eqArrays(arr1,arr2){
+  if (arr1.length==arr2.length){                  //comparing lengths
+    for(let i=0;i<arr1.length;i++)
+      {
+        if(arr1[i]!==arr2[i]){
+          return(false);
+        }
+      }
+      return(true);
+  }  
+  else {
+    return(false);
+  }
+};
+
+//Function to return an assert message
+
+function assertArrayequals(arr1,arr2){
+  if (eqArrays(arr1,arr2)===true) {
+    console.log(`Assertion passed:[ ${arr1}] === [${arr2}]`);
+  }
+  else {
+    console.log(`Asserion failed:[${arr1}]!==[${arr2}]`);
+  }
+
+}
+
+assertArrayequals([1, 2, 3], [1, 2, 3]);
+assertArrayequals([1, 2, 3], [3, 2, 1]);
+assertArrayequals(["1", "2", "3"], ["1", "2", "3"]);
+assertArrayequals(["1", "2", "3"], ["1", "2", 3]);
