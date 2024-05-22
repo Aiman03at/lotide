21287d871f66d7eacb0c1ca6839193421eae7753
Aiman03at <aiman03at@gmail.com>
2024-05-21 23:19:33 -0400

    Flattening Array

---
+++ flatten.js
@@ -0,0 +1,25 @@
+//Flatten a multidimensional array into one -dimensional
+//Input:Multidimensional array
+//A flat Array
+
+function flatten(arr){
+  const res=[]
+  for (item of arr){
+    if(Array.isArray(item))
+      {
+        for(ele of item){
+          res.push(ele)
+          
+        }
+
+        }
+      
+    else{
+      res.push(item)
+    } 
+  }
+    return (res); 
+  }
+
+  const r=flatten([1, 2, [3, 4], 5, [6]]);
+  console.log(r);
