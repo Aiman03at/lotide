ac86518cb7dbc872938121acf66f11c7794eecb9
Aiman03at <aiman03at@gmail.com>
2024-06-05 15:28:54 -0400

    Refactoring AssertArraysEqual.s

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,20 +4,7 @@
 //Using already implemented eqArrays
 
 // Function to evaluate whether two arrays are a perfect match
-function eqArrays(arr1,arr2){
-  if (arr1.length==arr2.length){                  //comparing lengths
-    for(let i=0;i<arr1.length;i++)
-      {
-        if(arr1[i]!==arr2[i]){
-          return(false);
-        }
-      }
-      return(true);
-  }  
-  else {
-    return(false);
-  }
-};
+const eqArrays=require("./eqArrays")
 /**
  * 
  * @param {Array} arr1 
@@ -26,17 +13,14 @@ function eqArrays(arr1,arr2){
 
 //Function to return an assert message
 
-function assertArrayequals(arr1,arr2){
+function assertArraysEqual(arr1,arr2){
   if (eqArrays(arr1,arr2)===true) {
     console.log(`✅✅✅Assertion passed:[ ${arr1}] === [${arr2}]`);
   }
   else {
-    console.log(`🛑🛑🛑Asserion failed:[${arr1}]!==[${arr2}]`);
+    console.log(`🛑🛑🛑Assertion failed:[${arr1}]!==[${arr2}]`);
   }
 
 }
 
-assertArrayequals([1, 2, 3], [1, 2, 3]);
-assertArrayequals([1, 2, 3], [3, 2, 1]);
-assertArrayequals(["1", "2", "3"], ["1", "2", "3"]);
-assertArrayequals(["1", "2", "3"], ["1", "2", 3]);
+module.exports=assertArraysEqual;
