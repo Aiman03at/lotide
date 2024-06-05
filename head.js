9a4eee09c0297c83ab1e85cd33630489bc318df2
Aiman03at <aiman03at@gmail.com>
2024-06-05 16:04:41 -0400

    Adding mocha and chai to headTest.js

--- head.js
+++ head.js
@@ -21,7 +21,5 @@ const head=function(arr){
 
 }
 // TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports=head;
 
