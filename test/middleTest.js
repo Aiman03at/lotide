190b55da276ff7d68bde872308b1de4b628ff9bb
Aiman03at <aiman03at@gmail.com>
2024-06-05 15:43:15 -0400

    Refactoring middle.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,5 @@
 const middle=require("../middle");
-const assertArrayequals=require("../assertArraysEqual")
+const assertArraysEqual=require("../assertArraysEqual")
 
 //TEST DATA 
 //checking the middle function
@@ -10,10 +10,10 @@ middle([1, 2, 3, 4, 5]); // => [3]
 
 //TEST DATA
 //Asserting the output
-assertArrayequals(middle([1, 2, 3, 4]),[2, 3]);
-assertArrayequals(middle([1, 2, 3, 4,5,6]),[3, 4]);
-assertArrayequals(middle([1, 2, 3, 4]),[ 3]);
-assertArrayequals(middle([1, 2, 3, 4,5]),[2, 3]);
-assertArrayequals(middle([1]),[]);
-assertArrayequals(middle([1, 2]),[]);
-assertArrayequals(middle([1, 2]),[2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4,5,6]),[3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4]),[ 3]);
+assertArraysEqual(middle([1, 2, 3, 4,5]),[2, 3]);
+assertArraysEqual(middle([1]),[]);
+assertArraysEqual(middle([1, 2]),[]);
+assertArraysEqual(middle([1, 2]),[2, 3]);
