ae8c48000fb24d37f86e8a67b1073096ae73c069
Aiman03at <aiman03at@gmail.com>
2024-06-05 22:33:41 -0400

    Refactoring tail.js using Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -4,25 +4,32 @@ const middle=require("../middle");
 
 
 describe("#middle", () => {
-  it("returns [2,3] for [1, 2, 3,4]", () => {
+  it ("returns [2,3] for [1, 2, 3,4]", () => {
     assert.deepEqual(middle([1, 2, 3,4]), [2,3]);
     });
-    it("returns [3,4]  for [1,2,3,4,5,6]", () => {
+    it ("returns [3,4]  for [1,2,3,4,5,6]", () => {
       assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]); 
     });  
-    it("returns [2] for [1,2,3]", () => {
+    it ("returns [2] for [1,2,3]", () => {
       assert.deepEqual(middle([1,2,3]), [2]); 
     });  
-    it("returns [3] for [1,2,3,4,5]", () => {
+    it ("returns [3] for [1,2,3,4,5]", () => {
       assert.deepEqual(middle([1,2,3,4,5]),[3]); 
     });  
     
-    it("returns []  for [1]", () => {
+    it ("returns []  for [1]", () => {
       assert.deepEqual(middle([1]), []); 
     }); 
     it("returns []  for [1,2]", () => {
       assert.deepEqual(middle([1,2]), []); 
     });   
+    // checking if original array is alterd
+    it("make sure the original array was not altered by the middle function", () => {
+      const words = ["hello", "world", "lighthouse"];
+      const result = middle(words);
+      assert.deepEqual(result, ["world"]);
+      assert.strictEqual(words.length, 3);
+    });
 
 });
 
